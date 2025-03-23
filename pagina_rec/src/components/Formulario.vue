280360ce27ccc5e58946d824d6c367ebd8176567
santiagomary1004 <22620262@tlaxiaco.tecnm.mx>
2025-03-23 15:29:48 -0600

    Agregando formulario

---
+++ pagina_rec/src/components/Formulario.vue
@@ -0,0 +1,32 @@
+<script>
+// export default {
+//     data(){
+//         return{
+//             formulario
+//         }
+//     }
+// }
+</script>
+<template>
+    <div class="form-container">
+        <h2>Iniciar Sesión</h2>
+        <form action="#" method="post">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" required>
+
+            <label for="apellido_paterno">Apellido Paterno:</label>
+            <input type="text" id="apellido_paterno" name="apellido_paterno" required>
+
+            <label for="apellido_materno">Apellido Materno:</label>
+            <input type="text" id="apellido_materno" name="apellido_materno" required>
+
+            <label for="email">Correo Electrónico:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="password">Contraseña:</label>
+            <input type="password" id="password" name="password" required>
+
+            <input type="submit" value="Iniciar sesión">
+        </form>
+    </div>
+</template>
