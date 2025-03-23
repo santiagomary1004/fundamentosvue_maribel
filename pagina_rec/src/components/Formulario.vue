ccf0caa22445753e5266ce66b55074803c922e1e
santiagomary1004 <22620262@tlaxiaco.tecnm.mx>
2025-03-23 15:43:19 -0600

    Agregando estilos al formulario

--- pagina_rec/src/components/Formulario.vue
+++ pagina_rec/src/components/Formulario.vue
@@ -30,3 +30,58 @@
         </form>
     </div>
 </template>
+
+<style>
+body {
+    font-family: Cambria, serif;
+    background-color: #f4f4f4;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+.form-container {
+    background-color: #fff;
+    padding: 70px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    width: 300px;
+}
+.form-container h2 {
+    margin-bottom: 20px;
+    font-size: 24px;
+    text-align: center;
+}
+.form-container label {
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
+}
+.form-container input[type="text"],
+.form-container input[type="email"],
+.form-container input[type="password"] {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 15px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+}
+.form-container input[type="submit"] {
+    width: 100%;
+    padding: 10px;
+    background-color: #AD2E05;
+    border: none;
+    border-radius: 4px;
+    color: #fff;
+    font-size: 16px;
+    cursor: pointer;
+}
+.form-container input[type="submit"]:hover {
+    background-color: #AD2E05;
+}
+
+  
+</style>
